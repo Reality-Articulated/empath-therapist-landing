38bea27bfbaae97c5cd1e405dbb3453e13f7eeb3
Karan <karanravindrapatil@gmail.com>
2025-01-16 20:24:13 +0530

    changed log in link to app.empathdash.com

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
               Advisory Program
             </Link>
             <a 
-  href="https://www.empathdash.com/login" 
+  href="https://app.empathdash.com/login" 
   target="_blank" 
   rel="noopener noreferrer" 
   className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors"
